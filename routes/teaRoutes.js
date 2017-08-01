ed10ff6f24b38e69f075f0da270ae0a33bb1532e
unknown <oscarhilton@hotmail.com>
2017-08-01 18:28:46 +0100

    Added tea routess

--- routes/teaRoutes.js
+++ routes/teaRoutes.js
@@ -3,10 +3,10 @@ function tea(title, score) {
   this.score = score;
 }
 
-var gunpowder = new tea('gunpowder', 9);
-var peach = new tea('peach', 8);
-var green = new tea('green', 5);
-var white = new tea('white', 7);
+var gunpowder = new tea('Gunpowder', 9);
+var peach = new tea('Peach', 8);
+var green = new tea('Green', 5);
+var white = new tea('White', 7);
 
 module.exports = app => {
   app.get('/api/teas', (req, res) => {
